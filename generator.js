2b187e8e14583a2ea5e232901dc54d67d06cc1b6
Ctucker9233 <court.m.tucker@gmail.com>
2019-11-10 15:06:45 -0800

    Updating code to simplify and meet ES6 standards

--- generator.js
+++ generator.js
@@ -1,67 +1,67 @@
-function generate(){
-    var randomCharString = "";
-    var specChar = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-    var numChar = "0123456789";
-    var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-    var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    //prompt user to pick a password length between 8 and 128 characters
-    var passwordLength = prompt("Please select a password length between 8 and 128 characters.");
-    var passwordString = "";
+let randomCharString = "";
+let charSet = [
+    {
+        char: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+        name: "Special Characters",
+        use: false
+    },
+    {
+        char: "0123456789",
+        name: "Number Characters",
+        use: false
+    },
+    {
+        char: "abcdefghijklmnopqrstuvwxyz",
+        name: "Lowercase Letters",
+        use: false
+    },
+    {
+        char: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        name: "Uppercase Letters",
+        use: false
+    }
+]
+
+//prompt user to pick a password length between 8 and 128 characters
+
+
+const generate = () => {
+    let passwordLength = parseInt(prompt("Please select a password length between 8 and 128 characters."));
+    let passwordString = "";
 
     //check if password is long enough
-    if(passwordLength > 8 && passwordLength < 128){
-        //ask user if they want to use special characters
-        var useSpecChar = (prompt("Do you want to use special characters?")).toLowerCase();
-        //add special characters to the string if user selects
-        if (useSpecChar === "yes" || useSpecChar === "y"){
-            randomCharString = randomCharString + specChar;
-            console.log(randomCharString);
-        }
-
-        //ask user if they want to use numerical characters
-        var useNumChar = (prompt("Do you want to use number characters?")).toLowerCase();
-        //add number characters to the string if user selects
-        if (useNumChar === "yes" || useNumChar === "y"){
-            randomCharString = randomCharString + numChar;
-            console.log(randomCharString);
-        }
-
-        //ask user if they want to use lowercase letters
-        var useLowerChar = (prompt("Do you want to use lowercase characters?")).toLowerCase();
-        //add lowercase characters to the string if user selects
-        if (useLowerChar === "yes" || useLowerChar === "y"){
-            randomCharString = randomCharString + lowerChar;
-            console.log(randomCharString);
-        }
-
-        //ask user if they want to use uppercase letters
-        var useUpperCase = (prompt("Do you want to use uppercase characters?")).toLowerCase();
-        //add uppercase characters to the string if users selects
-        if (useUpperCase === "yes" || useUpperCase === "y"){
-            randomCharString = randomCharString + upperChar;
-            console.log(randomCharString);
-        }  
+    if (passwordLength > 8 && passwordLength < 128) {
+        charSet.forEach(set => {
+            const useChar = (prompt(`Do you want to use ${set.name}?`)).toLowerCase();
+            if (useChar === "yes" || useChar === "y") {
+                set.use = true;
+            }
+            if(set.use){
+                randomCharString = randomCharString + set.char;
+            }
+        });
+        console.log(JSON.stringify(charSet))
     }
-    else{
+    else {
         alert("Your password does not meet the requirements. Please refresh and try again.");
     }
 
     //generate random password based on characters the user wants to use 
-    if(randomCharString !== ""){
-        for (i=1; i<=passwordLength; i++){
+    if (randomCharString !== "") {
+        for (i = 1; i <= passwordLength; i++) {
             passwordString = passwordString + randomCharString.charAt(Math.floor(Math.random() * Math.floor((randomCharString.length) - 1)));
         }
 
         document.getElementById("password").value = passwordString;
     }
-    else{
+    else {
         alert("You must use at least one kind of character. Please refresh and try again.");
     }
-}  
+}
 
-function copy(){
-    var copyText = document.querySelector("#password");
+const copy = () => {
+    const copyText = document.querySelector("#password");
     copyText.select();
     document.execCommand("copy");
-    alert("Your password has been copied to the clipboard.");   
+    alert("Your password has been copied to the clipboard.");
 }
